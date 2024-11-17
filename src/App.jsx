a60638c288a213d5cff6b51b26822e1557916fe9
abdessamieaksas <127380411+abdessamieaksas@users.noreply.github.com>
2024-11-18 00:07:39 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/login';
-import Layout from './components/Layout';
+
 
 function App() {
   return (
    
-    <Router>
-      <Routes>
-        {/* Route for login */}
-        <Route path="/" element={<Login />} />
-        
-        {/* Route for dashboard */}
-        <Route path="/dashboard" element={<Layout />} />
-      </Routes>
-    </Router>
+    <Login/>
   );
 }
 
